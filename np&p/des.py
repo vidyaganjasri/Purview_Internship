e33b8f04fc564104fee643d24786e6b728b9bcef
vidyaganjasri <134138048+vidyaganjasri@users.noreply.github.com>
2025-05-08 10:12:35 +0530

    Update and rename Numpy_Pandas to np&p/des.py

---
+++ np&p/des.py
@@ -0,0 +1,23 @@
+
+NumPy in a Nutshell:
+Purpose: Used for fast, efficient calculations with large datasets.
+
+Key Applications:
+Machine Learning (e.g., stock prediction models)
+Image Processing (working with 2D or 3D arrays like images)
+
+Why NumPy:
+Traditional Python lists are slow and inefficient when working with large volumes of data.
+NumPy arrays are much faster and memory-efficient because they are designed for numerical and scientific computing.
+
+Pandas in a Nutshell:
+Purpose: Built on top of NumPy, Pandas is for data analysis.
+
+Key Applications:
+Working with structured/tabular data (like data in Excel, CSV files, or databases).
+Data Cleaning: Removing missing values, handling duplicates.
+Data Manipulation: Summarizing, filtering, and grouping data.
+
+Why Pandas:
+While NumPy is focused on calculations and mathematical operations, Pandas excels in data analysis tasks.
+It internally uses NumPy to handle computations but provides easy-to-use structures like DataFrames for handling and analyzing structured data.
